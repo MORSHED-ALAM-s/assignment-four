521225971b95c6348e23fd75ebd11c6e275e5b92
MORSHED-ALAM-s <malamidb90@gmail.com>
2024-09-11 06:57:53 +0600

    first commit

---
+++ assingmet.js
@@ -0,0 +1,42 @@
+// Problem 1
+
+
+function calculateTax(income, expenses) {
+    
+}
+
+
+
+// problem 2
+
+
+function sendNotification(email) {
+    
+}
+
+
+// problem 3
+
+
+function checkDigitsInName(name) {
+    
+}
+
+
+
+// problem 4
+
+
+function calculateFinalScore(obj) {
+    
+}
+
+
+
+// problem 5
+
+
+function  waitingTime(waitingTimes  , serialNumber) {
+    
+}
+
